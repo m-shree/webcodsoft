777da707c9418558449320f39430023f85b69271
m-shree <mjeevithashree@gmail.com>
2024-11-12 22:09:31 +0530

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,9 @@
+// Open Side Navbar
+function openNavbar() {
+    document.getElementById("sideNavigationBar").style.width = "250px";
+}
+
+// Close Side Navbar
+function closeNavbar() {
+    document.getElementById("sideNavigationBar").style.width = "0";
+}
